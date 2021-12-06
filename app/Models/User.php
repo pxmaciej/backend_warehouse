f889244246f53ced825296ad97e079e179546784
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2021-12-06 17:46:35 +0100

    add User model and migration phone number

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'role',
+        'phone',
         'password',
     ];
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('role')->default('user');
+            $table->bigInteger('phone')->nullable()->default(48);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
